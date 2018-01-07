d61e5af0b55ee940542824cd4a0a321397cd9eaf
Nasir Taha <nasirtaha@gmail.com>
2018-01-07 13:46:30 +0100

    Add customization for all parts

---
+++ lang/en/theme_sodan.php
@@ -0,0 +1,69 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'theme_sudan', language 'en'
+ *
+ * @package   theme_sudan
+ * @copyright 2017 Moodle, moodle.org
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['choosereadme'] = '
+<div class="clearfix">
+<div class="well">
+<h2>sudan</h2>
+<p><img class=img-polaroid src="sudan/pix/screenshot.jpg" /></p>
+</div>
+<div class="well">
+<h3>About</h3>
+<p>Sudan theme designed to be standard  for all universities in Sudan. It inherits styles and renderers from moodle standard theme with many modifications.</p>
+<h3>Parents</h3>
+<p>This theme is based upon the Bootstrap theme, which was created for Moodle 2.5 and new versions.</p>
+<h3>Theme Credits</h3>
+<p>Authors: Nasir Saleh<br>
+Contact: nasir.saleh@student.fh-kiel.de<br>
+Website: <a href="https://www.linkedin.com/in/nasirtaha/">Linkedin profile</a>
+</p>
+<h3>Report a bug:</h3>
+<p><a href="https://github.com/NasirtahaReda/SudanTheme">https://github.com/NasirtahaReda/SudanTheme</a></p>
+<h3>More information</h3>
+</div></div>';
+
+$string['configtitle'] = 'sudan';
+
+$string['customcss'] = 'Custom CSS';
+$string['customcssdesc'] = 'Whatever CSS rules you add to this textarea will be reflected in every page, making for easier customization of this theme.';
+
+$string['footnote'] = 'Footnote';
+$string['footnotedesc'] = 'Whatever you add to this textarea will be displayed in the footer throughout your Moodle site.';
+
+$string['invert'] = 'Invert navbar';
+$string['invertdesc'] = 'Swaps text and background for the navbar at the top of the page between black and white.';
+
+$string['logo'] = 'Logo';
+$string['logodesc'] = 'The logo is only displayed in the header of the front page and login page.<br /> If the height of your logo is more than 75px add div.logo {height: 100px;} to the Custom CSS box below, amending accordingly if the height is other than 100px. If a logo is not added here, the logo in Appearance > Logos will be used.';
+
+$string['pluginname'] = 'sudan';
+
+$string['region-side-post'] = 'Right';
+$string['region-side-pre'] = 'Left';
+
+$string['sitelogo'] = 'Site logo';
+$string['sitename'] = 'Display site name along with small logo';
+$string['sitenamedesc'] = 'If there is no small logo, the site name is always displayed in the navigation bar. If a small logo is set, it may be displayed with or without the site name.';
+$string['smalllogo'] = 'Small logo';
+$string['smalllogodesc'] = 'The small logo is displayed in the navigation bar. If there is a header logo for the front page and login page, the small logo is not displayed on these pages. If a logo is not added here, the compact logo in Appearance > Logos will be used.';
